fe04d463348418c4990cd37243b1dbfb69f381f9
rajeshc1988 <36516359+rajeshc1988@users.noreply.github.com>
2020-02-24 10:21:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+
+app.post('/validate', function (req, res) {
+    let result = {
+        status:"Success",
+        code:200
+        
+    }
+    res.send(result)
+})
+
+//Launch listening server on port 8080
+app.listen(8080, function () {
+    console.log('App listening on port 8080!')
+})
